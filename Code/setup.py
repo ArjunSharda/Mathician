6281a9222ee9cc4a51660977e39ad529a7e1ac8f
Arjun Sharda <77706434+ajsharda17@users.noreply.github.com>
2022-06-13 16:11:32 -0500

    Update setup.py

--- Code/setup.py
+++ Code/setup.py
@@ -5,7 +5,7 @@ classifiers = [
   'Intended Audience :: Education',
   'Operating System :: Microsoft :: Windows :: Windows 10',
   'License :: OSI Approved :: MIT License',
-  'Programming Language :: Python :: 3.8'
+  'Programming Language :: Python :: 3.10'
 ]
  
 setup(
