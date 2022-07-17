be05e5bb034f1e39021d4ac27ee2e8f9217a79e0
Arjun Sharda <77706434+ArjunSharda@users.noreply.github.com>
2022-07-17 12:51:32 -0500

    Update setup.py

--- Code/setup.py
+++ Code/setup.py
@@ -5,12 +5,14 @@ classifiers = [
   'Intended Audience :: Education',
   'Operating System :: Microsoft :: Windows :: Windows 10',
   'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3.8',
+  'Programming Language :: Python :: 3.9',
   'Programming Language :: Python :: 3.10'
 ]
  
 setup(
   name='Mathician',
-  version='1.4.3',
+  version='1.4.5',
   description='Math package',
   long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
   url='https://github.com/ajsharda17/Mathician',  
